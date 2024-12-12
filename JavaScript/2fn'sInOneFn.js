46158c5430cb2700ab808f03f7ab933c911bc5bf
Sri Vaishnavi <srivaishnavi0602@gmail.com>
2024-12-12 21:49:40 +0530

    Create 2fn'sInOneFn.js
    Understanding nested functions in js.

---
+++ JavaScript/2fn'sInOneFn.js
@@ -0,0 +1,19 @@
+// This program helps user to understand how to write 2 or more functions in js
+
+var expect = function(val) {
+    return {
+        toBe: (val2) => {
+            if (val !== val2) throw new Error("Not Equal");
+            else return true;
+        },
+        notToBe: (val2) => {
+            if (val === val2) throw new Error("Equal");
+            else return true;
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
